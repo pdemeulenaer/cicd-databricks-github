1d4df92f53920805dca331c2d3ea9aeb7bb57800
Philippe de Meulenaer <31767740+pdemeulenaer@users.noreply.github.com>
2021-11-08 17:50:36 +0200

    fix missing json package

--- cicd_databricks_github/jobs/sample/entrypoint.py
+++ cicd_databricks_github/jobs/sample/entrypoint.py
@@ -3,6 +3,7 @@ from cicd_databricks_github.common import Job
 import pandas as pd
 import numpy as np
 import mlflow
+import json
 
 #Import of SKLEARN packages
 from sklearn.metrics import accuracy_score, roc_curve, auc, confusion_matrix
