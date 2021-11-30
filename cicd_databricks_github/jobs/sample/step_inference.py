d4a63e3cf4a36568e302f74ab9a25301c2a1328f
Philippe de Meulenaer <31767740+pdemeulenaer@users.noreply.github.com>
2021-11-30 22:17:45 +0200

    correct model loading from mlflow in inference code

--- cicd_databricks_github/jobs/sample/step_inference.py
+++ cicd_databricks_github/jobs/sample/step_inference.py
@@ -81,9 +81,9 @@ class SampleJob(Job):
         # Get experiment and runs 
         exp  = client.get_experiment_by_name(experiment)
         runs = mlflow.search_runs([exp.experiment_id], "", order_by=["metrics.Accuracy DESC"], max_results=1)
-        best_run = runs[0]
+        best_run_id = runs["run_id"][0]
 
-        model_path = "runs:/{0}/model".format(best_run)
+        model_path = "runs:/{0}/model".format(best_run_id)
         model = mlflow.pyfunc.load_model(model_path)
         y_test_pred = model.predict(pd.DataFrame(x_test))                    
 
