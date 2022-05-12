cc9848182f78c955fd0cf9bd94f0ee556da2affd
Philippe de Meulenaer <31767740+pdemeulenaer@users.noreply.github.com>
2022-05-12 11:00:07 +0300

    modify setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from cicd_databricks_github import __version__
 
 setup(
     name="cicd_databricks_github",
-    packages=find_packages(exclude=["tests", "tests.*"]),
+    packages=find_packages(exclude=["tests", "tests.*", "dags", "notebooks"]),
     setup_requires=["wheel"],
     version=__version__,
     description="Databricks Labs CICD Templates Sample Project",
-    author="",
+    author="Philippe de Meulenaer",
 )
