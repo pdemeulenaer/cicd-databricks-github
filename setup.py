86530edda13d9bd9332f84eb2202ec04747dbff0
Philippe de Meulenaer <31767740+pdemeulenaer@users.noreply.github.com>
2022-05-19 11:11:55 +0300

    add scikit-learn constraint in reqs

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from cicd_databricks_github import __version__
 setup(
     name="cicd_databricks_github",
     packages=find_packages(exclude=["tests", "tests.*", "dags", "notebooks"]),
-    setup_requires=["wheel","Cython>=0.29.24","scikit-learn<1.1.0","evidently"],
+    setup_requires=["wheel","evidently"],
     version=__version__,
     description="Databricks Labs CICD Templates Sample Project",
     author="Philippe de Meulenaer",
