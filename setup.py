d251faf6535805119ccda790818f8669e248d662
Philippe de Meulenaer <31767740+pdemeulenaer@users.noreply.github.com>
2022-05-19 11:00:04 +0300

    fix scikit-learn dependency in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from cicd_databricks_github import __version__
 setup(
     name="cicd_databricks_github",
     packages=find_packages(exclude=["tests", "tests.*", "dags", "notebooks"]),
-    setup_requires=["wheel","evidently"],
+    setup_requires=["wheel","scikit-learn<1.1.0","evidently"],
     version=__version__,
     description="Databricks Labs CICD Templates Sample Project",
     author="Philippe de Meulenaer",
